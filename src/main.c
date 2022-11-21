9d35b44817eb9a450de8020ef0e9d62e2e75efec
agamandika99 <117974350+agamandika99@users.noreply.github.com>
2022-11-21 12:25:29 +0700

    Copying blueprints from Agam's device

--- src/main.c
+++ src/main.c
@@ -101,3 +101,63 @@ int main(){
 	int numChoosed;
 	Pilih_Menu(&numChoosed);
 }
+
+
+// BLUEPRINTS! DO NOT UN-COMMENT THESE!!!
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <stdlib.h>
+
+// typedef struct {
+// 	int daNum;
+// 	char* daName;
+// } daList;
+
+// int main(){
+// 	daList menuList[5];
+
+// 	for(int i=0; i<5; i++){
+// 		menuList[i].daNum = i+1;
+// 		menuList[i].daName = "Hello World";
+// 	}
+// 	printf("============================================\n");
+// 	for(int i=0; i<5;i++){
+// 		printf("           %d. %s                \n", menuList[i].daNum, menuList[i].daName);
+// 	}
+// 	printf("============================================\n");
+// }
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <stdbool.h>
+// #include <string.h>
+
+// int metricMeasure(int* size, bool* doesIncrease){
+// 	if(doesIncrease){
+// 		return *size * 10;
+// 	}else{
+// 		return *size * 0.1;
+// 	}
+// }
+
+// int main(){
+// 	int num; bool measureChange; char sat[] = "";
+// 	char* satuan[] = {"k", "h", "da", "\0", "d", "c", "m"};
+// 	printf("Masukkan besaran serta satuannya: ");
+// 	scanf("%d", &num); fgets(sat, sizeof(sat), stdin);
+// 	int len = sizeof(satuan)/sizeof(satuan[0]);
+// 	int x;
+// 	for(int i = 0; i < len; ++i){
+// 	    if(!strcmp(satuan[i], sat)){
+// 	    	printf("Okay!\n");
+// 	        // Do your stuff
+// 	    }
+// 	}
+// 	printf(""); puts(sat);
+// //	num = 1;
+// 	for(int i=0; i<7; i++){
+// 		printf("%d %sm\n", num, satuan[i]);
+// 		num = metricMeasure(&num, &measureChange);
+// 	}
+// 	return 0;
+// }
